d89b55cdae3a5ad11f5a79769ee9ee089b002a84
haikallf <haikallzrd@gmail.com>
2023-05-18 06:41:35 +0700

    init

---
+++ SleepGuard Watch App/SleepGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepGuardApp.swift
+//  SleepGuard Watch App
+//
+//  Created by Haikal Lazuardi on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepGuard_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepGuard/SleepGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepGuardApp.swift
+//  SleepGuard
+//
+//  Created by Haikal Lazuardi on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
