efd8ec5062afd4beb354a6611dd289ec7db9a477
haikallf <haikallzrd@gmail.com>
2023-05-18 21:10:52 +0700

    fully functioned views

---
+++ SleepGuard/Enum/WakeUpType.swift
@@ -0,0 +1,13 @@
+//
+//  WakeUpType.swift
+//  SleepGuard
+//
+//  Created by Haikal Lazuardi on 18/05/23.
+//
+
+import SwiftUI
+
+enum WakeUpType: String, CaseIterable {
+    case StandUp = "Stand Up"
+    case Walk = "Walk"
+}
--- SleepGuard/Views/ChangeWakeUpModal.swift
+++ SleepGuard/Views/ChangeWakeUpModal.swift
@@ -12,7 +12,10 @@ struct ChangeWakeUpModal: View {
     
     @Binding var wakeUpTime: Date?
     @State var selectedDate = Date()
-    let dateFormatter = DateFormatter()
+    @State var wakeUpType: String = WakeUpType.StandUp.rawValue
+    @State var standUpDuration: Int = 30
+    @State var walkSteps: Int = 10
+    let wakeUpTypes: [WakeUpType] = WakeUpType.allCases
     
     func formatTime(time: Date) -> String {
         let dateFormatterTemplate = DateFormatter()
@@ -31,13 +34,65 @@ struct ChangeWakeUpModal: View {
             .datePickerStyle(WheelDatePickerStyle())
             .labelsHidden()
             
-            Text(formatTime(time: selectedDate))
+//            Text(formatTime(time: selectedDate))
+            
+            VStack {
+                HStack {
+                    Text("Awake Confirmation")
+                    Spacer()
+                    Picker("", selection: $wakeUpType) {
+                        ForEach(wakeUpTypes, id: \.self) {elmt in
+                            Text(elmt.rawValue)
+                                .tag(elmt.rawValue)
+                        }
+                    }
+                    .foregroundColor(.white)
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("\(wakeUpType == WakeUpType.StandUp.rawValue ? "Duration" : "Steps")")
+                    Spacer()
+                    if (wakeUpType == WakeUpType.StandUp.rawValue) {
+                        Picker("", selection: $standUpDuration) {
+                            ForEach(1..<6, id: \.self) {elmt in
+                                Text("\(elmt * 30)s")
+                                    .tag(elmt * 30)
+                            }
+                        }
+                    } else {
+                        Picker("", selection: $walkSteps) {
+                            ForEach(1..<6, id: \.self) {elmt in
+                                Text("\(elmt * 10)")
+                                    .tag(elmt * 10)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color("gray"))
+            .cornerRadius(10)
+            
+            Button(action: {
+                
+            }) {
+                Text("Delete Alarm")
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.red)
+                    .background(Color("gray"))
+                    .cornerRadius(10)
+            }
+            .padding(.top, 40)
             
             Spacer()
         }
+        .padding()
 //        .onAppear {
-//            wakeUpTime = wakeUpTime ?? Date()
-//        }
+//            print(wakeUpTypes)        }
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Text("Cancel")
